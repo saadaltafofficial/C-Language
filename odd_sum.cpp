63969c477a4aabbc85d8539bdcbe9c99ed9e56be
Saad Altaf <134957807+saadaltafofficial@users.noreply.github.com>
2025-01-22 16:04:09 +0500

    Update odd_sum.cpp

--- odd_sum.cpp
+++ odd_sum.cpp
@@ -23,3 +23,30 @@ int main() {
     cout << "Therefore the odd sum of numbers to upperLimit you provide is " << sum << endl;
   }
 }
+
+// Calculate the even and odd sum both
+#include <iostream>
+using namespace std;
+int main() {
+  int evenSum, oddSum, number, upperLimit;
+  
+  number = 1;
+
+  cout << "Please enter a number: ";
+  cin >> upperLimit;
+  if(upperLimit <= 0){
+    cout << "The upperLimit should always be greater then 0"<< endl;
+  } else {
+    while(number <= upperLimit){
+      if((number % 2) != 0){
+        oddSum = oddSum + number;
+      } else {
+        evenSum = evenSum + number;
+      }
+      
+      number = number + 1;
+    }
+    cout << "The even Sum is " << evenSum << endl;
+    cout << "The odd Sum is " << oddSum << endl;
+  }
+}
